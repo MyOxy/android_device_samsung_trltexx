37b230a3b443985d5a17a3d14baf09ecd81bcb29
MyOxy <51816859+MyOxy@users.noreply.github.com>
2020-05-18 17:22:02 +0200

    Update and rename full_trltexx.mk to lineage_trltexx.mk

--- lineage_trltexx.mk
+++ lineage_trltexx.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/trltexx/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_trltexx
+PRODUCT_NAME := lineage_trltexx
 PRODUCT_DEVICE := trltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
